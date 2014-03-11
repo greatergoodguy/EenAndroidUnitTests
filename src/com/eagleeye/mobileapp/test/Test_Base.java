25a15759bb7222f8c8a99ff828d60ee6018e31d0
Tom Yu <tom.yu@burstingbrains.com>
2014-03-11 10:55:02 -0500

    Fixed bug regarding adding bridges to the cameraIdsOnline

--- src/com/eagleeye/mobileapp/test/Test_Base.java
+++ src/com/eagleeye/mobileapp/test/Test_Base.java
@@ -1,10 +1,13 @@
 package com.eagleeye.mobileapp.test;
 
+import java.util.concurrent.CountDownLatch;
+
 import org.apache.http.Header;
 
 import android.test.ActivityInstrumentationTestCase2;
 
 import com.eagleeye.mobileapp.ActivityUnitTests;
+import com.eagleeye.mobileapp.pojo.PojoAaaAuthenticatePost;
 import com.eagleeye.mobileapp.util.UtilLogger;
 import com.eagleeye.mobileapp.util.http.UtilHttpAaa;
 import com.loopj.android.http.TextHttpResponseHandler;
@@ -12,8 +15,6 @@ import com.loopj.android.http.TextHttpResponseHandler;
 public abstract class Test_Base extends ActivityInstrumentationTestCase2<ActivityUnitTests> implements Constants {
 
 	protected final String TAG = getClass().getSimpleName();
-	
-	String authenticationToken = "";
 	
 	public Test_Base() {
 		super(ActivityUnitTests.class);
@@ -23,10 +24,46 @@ public abstract class Test_Base extends ActivityInstrumentationTestCase2<Activit
 	// Helper Methods
 	// =====================
 	public void login() {
+		final CountDownLatch latch = new CountDownLatch(1);
+		UtilHttpAaa.authenticatePost(TESTACCOUNT_USERNAME, TESTACCOUNT_PASSWORD, new TextHttpResponseHandler() {
+			@Override public void onSuccess(int statusCode, Header[] headers, String responseString) {
+				UtilLogger.logInfo(TAG, "Http Authenticate Post onSuccess()");
+				
+				PojoAaaAuthenticatePost pojo = new PojoAaaAuthenticatePost(responseString);
+				String authenticationToken = pojo.token;
+				
+				UtilHttpAaa.authorizePost(authenticationToken, new TextHttpResponseHandler() {
+					
+					@Override public void onSuccess(int statusCode, Header[] headers, String responseString) {
+						UtilLogger.logInfo(TAG, "Http Authorize Post onSuccess()");
+						latch.countDown();
+						
+					}
+					
+					@Override public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+						UtilLogger.logInfo(TAG, "Http Authorize Post onFailure()");
+						latch.countDown();
+					}
+				});
+				
+			}
+			
+			@Override
+			public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+				UtilLogger.logInfo(TAG, "Http Authenticate Post onFailure()");
+				latch.countDown();
+			}
+		});
 		
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void logout() {
+		final CountDownLatch latch = new CountDownLatch(1);
 		UtilHttpAaa.logoutPost(new TextHttpResponseHandler() {
 			@Override
 			public void onSuccess(int statusCode, Header[] headers, String responseString) {
@@ -37,7 +74,18 @@ public abstract class Test_Base extends ActivityInstrumentationTestCase2<Activit
 			public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
 				UtilLogger.logInfo(TAG, "Http Logout Post onSuccess()");
 			}
+			
+			@Override
+			public void onFinish() {
+				latch.countDown();
+			}
 		});
+		
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
